df3bac214054c2daeb2bed06bd12668268f02713
Tirth Patel <complaints@tirthpatel.com>
2015-05-16 19:58:59 -0400

    Added dialog for FB information, general clean up

--- MessageAnalyzer/MainWindow.xaml.cs
+++ MessageAnalyzer/MainWindow.xaml.cs
@@ -1,28 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MessageAnalyzer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void FbButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var dlg = new InfoDialogBox {Owner = this};
+
+            var result = dlg.ShowDialog();
+
+            StatusBlock.Text = result == true ? "Done." : "Nope.";
+        }
+
+        private void WaButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SmsButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
