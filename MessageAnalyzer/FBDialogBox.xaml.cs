d6aaa18100a2737b6b940da6e62a77ec35a1f941
Tirth Patel <complaints@tirthpatel.com>
2015-05-18 01:57:39 -0400

    Implemented FB message downloading

--- MessageAnalyzer/FBDialogBox.xaml.cs
+++ MessageAnalyzer/FBDialogBox.xaml.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
+using Newtonsoft.Json.Linq;
 
 namespace MessageAnalyzer
 {
@@ -13,8 +16,12 @@ namespace MessageAnalyzer
     {
         readonly static Uri BaseAddress = new Uri("https://www.facebook.com");
 
+        private const char Sepr = '|';
+        private const int MessageLimit = 5000;
+
         static HttpClient _client;
         private static string _accessToken;
+        private static string _myId;
 
         public InfoDialogBox()
         {
@@ -23,23 +30,22 @@ namespace MessageAnalyzer
             // to persist logged in session
             var cookieContainer = new CookieContainer();
 
-            var handler = new HttpClientHandler {CookieContainer = cookieContainer};
+            var handler = new HttpClientHandler { CookieContainer = cookieContainer };
 
-            _client = new HttpClient(handler) {BaseAddress = BaseAddress};
+            _client = new HttpClient(handler) { BaseAddress = BaseAddress };
             _client.DefaultRequestHeaders.Add("user-agent", "NCSA_Mosaic/2.0 (Windows 3.1)");
         }
 
         private void OkButton_OnClick(object sender, RoutedEventArgs e)
         {
-            var buttonText = ((Button) sender).Content.ToString();
-            SecondStatusBlock.Text = ((Button)sender).Content.ToString();
+            var buttonText = ((Button)sender).Content.ToString();
 
             if (buttonText == "Log in")  // first screen
             {
                 var email = EmailTextBox.Text;
                 var password = PasswordEntry.Password;
 
-                StatusBlock.Text = "Logging in...";
+//                StatusBlock.Text = "Logging in...";
 
                 // simple validity checks
                 if (!email.Contains("@"))
@@ -48,7 +54,9 @@ namespace MessageAnalyzer
                     StatusBlock.Text = "Invalid password";
                 else
                 {
-                    _accessToken = FB_login(EmailTextBox.Text, PasswordEntry.Password, _client);
+                    var fbInfo = FB_login(email, password, _client);
+                    _accessToken = fbInfo.Item1;
+                    _myId = fbInfo.Item2;
 
                     if (_accessToken.Length == 0)
                         StatusBlock.Text = "Login failed";
@@ -59,15 +67,31 @@ namespace MessageAnalyzer
                         DetailsStackPanel.Visibility = Visibility.Visible;
                         OkButton.Content = "Punch it!";
                     }
-                } 
+                }
             }
             else  // second screen
             {
-                SecondStatusBlock.Text = NameFromFbId(IdTextBox.Text, _client);
+                StatusBlock.Text = "Login successful!";
+
+                var isGroup = !FriendRadioButton.IsChecked.GetValueOrDefault();
+                var convoId = IdTextBox.Text;
+
+                var convoName = isGroup ?
+                    GroupNameFromFbId(convoId, _client) : FriendNameFromFbId(convoId, _client);
+
+                StatusBlock.Text = "Getting conversations with " + convoName;
+
+                // TODO: Check to make sure friend/group exists
+                var got = GetMessages(_client, _accessToken, _myId, convoId, convoName, isGroup, StatusBlock);
+
+                StatusBlock.Text = got;
+                OkButton.Visibility = Visibility.Hidden;
+                DetailsStackPanel.Visibility = Visibility.Collapsed;
+                CancelButton.Content = "Exit";
             }
         }
 
-        private static string FB_login(string username, string password, HttpClient client)
+        private static Tuple<string, string> FB_login(string username, string password, HttpClient client)
         {
             var loginPage = client.GetStringAsync(BaseAddress);
 
@@ -89,10 +113,13 @@ namespace MessageAnalyzer
             const string tokenSearch = @"fb_dtsg\\"" value=\\""(\w+?)\\";
             var token = Regex.Match(response, tokenSearch).Groups[1].Value;
 
-            return token;
+            const string myIdSearch = @"id=""profile_pic_header_(\d+?)""";
+            var myId = Regex.Match(response, myIdSearch).Groups[1].Value;
+
+            return new Tuple<string, string>(token, myId);
         }
 
-        private static string NameFromFbId(string id, HttpClient client)
+        private static string FriendNameFromFbId(string id, HttpClient client)
         {
             var personPage = client.GetStringAsync(BaseAddress + id).Result;
 
@@ -101,13 +128,115 @@ namespace MessageAnalyzer
 
             return name;
         }
+
+        private static string GroupNameFromFbId(string id, HttpClient client)
+        {
+            var groupPage = client.GetStringAsync(BaseAddress + "messages/conversation-" + id).Result;
+
+            var groupSearch = @"thread_fbid"":""" + id + @"(.*?)snippet";
+            var groupInfo = Regex.Match(groupPage, groupSearch).ToString();
+
+            const string nameSearch = @"name"":""(.*?)""";
+            var name = Regex.Match(groupInfo, nameSearch).Groups[1].Value;
+
+            return name;
+        }
+
+        private void radio_checked(object sender, RoutedEventArgs e)
+        {
+            // prevent initial NPE
+            if (ConvoTypeBlock == null)
+                return;
+
+            var checkedButton = ((RadioButton)sender).Content.ToString();
+
+            if (checkedButton == "Friend")
+                ConvoTypeBlock.Text = "Friend ID";
+            else if (checkedButton == "Group")
+                ConvoTypeBlock.Text = "Conversation ID";
+        }
+
+        private static string GetMessages(HttpClient client, string accessToken, string myId, string convoId,
+            string convoName, bool isGroup, TextBlock statusBlock)
+        {
+            var offset = 0;
+            var numMessages = 0;
+            var authorIds = new Dictionary<string, string> { { myId, "me" } };
+
+            using (var output = new StreamWriter(convoName + ".txt"))
+                while (true)
+                {
+                    var page = client.GetStringAsync(ConvoUrl(myId, convoId, accessToken, offset, isGroup)).Result;
+
+                    var messages = JObject.Parse(page.Substring(9))["payload"]["actions"].Children().ToList();
+                    numMessages += messages.Count;
+
+                    foreach (var message in messages)
+                    {
+                        var date = (message["timestamp"].ToString());
+                        var source = String.Join(" ", message["source_tags"]).Contains("mobile") ? "mobile" : "web";
+
+                        var authorId = message["author"].ToString().Split(':')[1];
+                        string author;
+
+                        try
+                        {
+                            author = authorIds[authorId];
+                        }
+                        catch (KeyNotFoundException)
+                        {
+                            author = FriendNameFromFbId(authorId, client);
+                            authorIds.Add(authorId, author);
+                        }
+
+                        var body = message["body"] ?? message["log_message_body"];
+                        body = body.ToString().Replace('\n', ' ');
+
+                        var location = message["coordinates"].ToString() == ""
+                            ? "No location"
+                            : message["coordinates"]["latitude"] + "," +
+                              message["coordinates"]["longitude"];
+
+                        if (message["has_attachment"] != null)
+                            foreach (var attachment in message["attachments"])
+                                body += attachment["attach_type"].ToString();
+
+                        // TODO: JSONify
+                        output.WriteLine(String.Join(Sepr.ToString(), date, author, body, source, location));
+                    }
+
+                    if (messages.Count < MessageLimit)
+                        break;
+
+                    offset += MessageLimit;
+                    statusBlock.Text = numMessages + " so far, getting more";
+                }
+
+            return String.Format("Got {0} messages with {1}", numMessages, convoName);
+
+//            foreach (var authorId in authorIds)
+//                Console.Out.WriteLine(authorId.ToString());
+        }
+
+        private static Uri ConvoUrl(string myId, string convoId, string accessToken, int offset, bool isGroup)
+        {
+            var convoUrl = String.Format("https://www.facebook.com/ajax/mercury/thread_info.php?" +
+                                         "&messages[user_ids][{1}][limit]={4}" +
+                                         "&messages[user_ids][{1}][offset]={2}" +
+                                         "&client=web_messenger&__user={0}&__a=1&fb_dtsg={3}",
+                                          myId, convoId, offset, accessToken, MessageLimit);
+            if (isGroup)
+                convoUrl = convoUrl.Replace("user_ids", "thread_fbids");
+
+            return new Uri(convoUrl);
+        }
     }
 
     public class EmailValidationRule : ValidationRule
     {
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            var email = (string) value;
+            var email = (string)value;
             if (email != null && !email.Contains("@"))
             {
                 return new ValidationResult(false, "Not a valid email.");
