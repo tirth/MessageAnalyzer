60f1d001f1ac13ca2eeb64f934df3714dd5e058b
Tirth Patel <complaints@tirthpatel.com>
2015-05-19 17:47:09 -0400

    Updated conversation sorting choices

--- MessageAnalyzer/FreqDialogBox.xaml.cs
+++ MessageAnalyzer/FreqDialogBox.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace MessageAnalyzer
 {
@@ -41,8 +30,44 @@ namespace MessageAnalyzer
 
         private void AnalyzeClick(object sender, RoutedEventArgs e)
         {
-            Stuff.ConvoFrequencyByDate(convoPicker.SelectedItem.ToString(), true);
+            var byLen = byLenRadio.IsChecked.GetValueOrDefault();
+
+            if (dateRangeRadio.IsChecked.GetValueOrDefault())
+            {
+                var fromD = fromDate.SelectedDate.GetValueOrDefault();
+                var toD = toDate.SelectedDate.GetValueOrDefault();
+
+                var data = Stuff.ConvoFrequencyByDate(convoPicker.SelectedItem.ToString(), true, byLen, 
+                    new []{fromD.Year, fromD.Month, fromD.Day}, new []{toD.Year, toD.Month, toD.Day});
+            }
+            else
+            {
+                var data = Stuff.ConvoFrequencyByDate(convoPicker.SelectedItem.ToString(), true, byLen);
+            }
+            
             StatusBlock.Text = "Analyzed " + convoPicker.SelectedItem;
+
+//            var plot = new PlotWindow() {Owner = this};
+//            var result = plot.ShowDialog();
+        }
+
+        public void display_radio(object sender, RoutedEventArgs e)
+        {
+            datePickPanel.Visibility = dateRangeRadio.IsChecked.GetValueOrDefault() ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public void convo_picked(object sender, RoutedEventArgs e)
+        {
+            var messages = File.ReadAllLines(convoPicker.SelectedItem + ".txt");
+
+            var fromTs = Convert.ToInt64(messages[0].Split(Stuff.Sepr)[0]);
+            var fromD = DateTimeOffset.FromUnixTimeMilliseconds(fromTs).ToLocalTime().DateTime;
+
+            var toTs = Convert.ToInt64(messages[messages.Length - 1].Split(Stuff.Sepr)[0]);
+            var toD = DateTimeOffset.FromUnixTimeMilliseconds(toTs).ToLocalTime().DateTime;
+
+            fromDate.SelectedDate = fromD;
+            toDate.SelectedDate = toD;
         }
     }
 }
---
+++ MessageAnalyzer/PlotWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace MessageAnalyzer
+{
+    public partial class PlotWindow : Window
+    {
+        public PlotModel MyModel { get; private set; }
+
+        public PlotWindow()
+        {
+            MyModel = new PlotModel {Title = "Test"};
+            MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+        }
+    }
+}
